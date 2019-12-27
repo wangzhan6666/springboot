9464f05425a927051a3ca1cafc9edcd10f3d34a1
WangZhan <2774891137@qq.com>
2019-12-27 15:36:30 +0800

    thymeleaf--version1.3

---
+++ src/main/java/com/wangzhan/springboot/test/Tews.java
@@ -0,0 +1,8 @@
+package com.wangzhan.springboot.test;
+
+/**
+ * @Author wangzhan
+ * @Date 2019/12/27  15:25
+ **/
+public class Tews {
+}
---
+++ src/main/java/com/wangzhan/springboot/test2/Test2.java
@@ -0,0 +1,8 @@
+package com.wangzhan.springboot.test2;
+
+/**
+ * @Author wangzhan
+ * @Date 2019/12/27  15:12
+ **/
+public class Test2 {
+}
