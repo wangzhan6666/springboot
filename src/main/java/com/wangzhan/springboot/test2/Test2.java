dd0fc3344da1579fe768680db219131640205cfa
WangZhan <2774891137@qq.com>
2019-12-27 15:54:06 +0800

    Test2.java--version1.2

--- src/main/java/com/wangzhan/springboot/test2/Test2.java
+++ src/main/java/com/wangzhan/springboot/test2/Test2.java
@@ -5,4 +5,17 @@ package com.wangzhan.springboot.test2;
  * @Date 2019/12/27  15:12
  **/
 public class Test2 {
+
+    public static void main(String[] args){
+        System.out.println("新年好呀！！！！！！！！！！");
+        System.out.println("新年好呀！！！！！！！！！！");
+        System.out.println("新年好呀！！！！！！！！！！");
+        System.out.println("新年好呀！！！！！！！！！！");
+        System.out.println("新年好呀！！！！！！！！！！");
+        System.out.println("新年好呀！！！！！！！！！！");
+        System.out.println("新年好呀！！！！！！！！！！");
+        System.out.println("新年好呀！！！！！！！！！！");
+
+
+    }
 }
